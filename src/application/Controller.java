83b377c1227ecc10d03c35a041485312a2377985
vinay <vinayyadav91190@gmail.com>
2023-05-23 16:35:43 +0530

    add Circle

--- src/application/Controller.java
+++ src/application/Controller.java
@@ -1,24 +1,36 @@
 package application;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.shape.Circle;
+
+
 
 public class Controller {
+	
+	@FXML
+	
+	private Circle myCircle;
+	private double x;
+	private double y;
+	
 	public void left(ActionEvent e)
 	{
-		System.out.println("left");
+		myCircle.setCenterX(x-=1);
 	}
 	
 	public void down(ActionEvent e)
 	{
-		System.out.println("down");	
+	
+		myCircle.setCenterY(y+=1);
 	}
 	
 	public void up(ActionEvent e)
 	{
-		System.out.println("up");
+	myCircle.setCenterY(y-=1);
 	}
 	public void right(ActionEvent e)
 	{
-		System.out.println("right");
+		myCircle.setCenterX(x+=1);
 	}
 }
